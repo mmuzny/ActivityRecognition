57a10761f75b6319e5b7c3104fae6de823930cdd
Miroslav Muzny <mmuzny@gmail.com>
2012-07-19 15:57:29 +0200

    3Dollar recognizer improvements

--- 3$Recognizer/ThreeDollarRecognizer.cs
+++ 3$Recognizer/ThreeDollarRecognizer.cs
@@ -14,41 +14,106 @@ namespace Classification
         public static int N = 64;
         
         //Expression tree build lambda function for subtraction
-        private static readonly Func<M, M, double> Sub;
+        private static readonly Func<M, M, double> Sub, Add, Mul, Div;
 
+        //Static class constructor
         static ThreeDollarRecognizer() {
             var firstOperand = Expression.Parameter(typeof(M), "i");
             var secondOperand = Expression.Parameter(typeof(M), "j");
             var body = Expression.Subtract(firstOperand, secondOperand);
+            var body_mul = Expression.Subtract(firstOperand, secondOperand);
+            var body_div = Expression.Subtract(firstOperand, secondOperand);
+            var body_add = Expression.Subtract(firstOperand, secondOperand);
             Sub = Expression.Lambda<Func<M, M, double>>
                   (body, firstOperand, secondOperand).Compile();
+            Div = Expression.Lambda<Func<M, M, double>>
+                  (body_div, firstOperand, secondOperand).Compile();
+            Add = Expression.Lambda<Func<M, M, double>>
+                  (body_add, firstOperand, secondOperand).Compile();
+            Mul = Expression.Lambda<Func<M, M, double>>
+                  (body_mul, firstOperand, secondOperand).Compile();
         }
 
-
+        //Distance computation between two points
         private double distance(Unistroke<M>.Point3<M> i,Unistroke<M>.Point3<M> j){
             return Math.Sqrt(Math.Pow(Sub(i.x, j.x), 2) + Math.Pow(Sub(i.y, j.y), 2) + Math.Pow(Sub(i.z, j.z), 2)); 
         }
 
+        //Determines length of whole path
         private double path_length(ref Unistroke<M> unistroke) {
-            for (int i = 0; i < unistroke.Length(); i++) { 
-                                       
+            double d = 0;
+
+            for (int i = 0; i < unistroke.Length()-1; i++) {
+                d += distance(unistroke[i], unistroke[i + 1]); 
             }
 
-            return 0;
+            return d;
         }
 
-        public void resample(T obj)
+        //Resamples points using linear interpolation
+        public void resample(ref T uni, out List<Unistroke<M>.Point3<M>> new_points)
         {
-            
+            new_points = new List<Unistroke<M>.Point3<M>>();
+            Unistroke<M> u = uni;
+            double d_seq = path_length(ref u) / (N - 1);
+            int i = 0; double d_curr = 0, d_comp = 0;
+            for (i=0; i<u.trace.Count; i++){
+                d_curr = distance(u[i], u[i + 1]);
+                if (d_curr + d_comp > d_seq)
+                {
+                    double x = Add(u[i - 1].x, (M)(object)((d_seq - d_comp) * Sub(u[i].x, u[i - 1].x)));
+                    double y = Add(u[i - 1].y, (M)(object)((d_seq - d_comp) * Sub(u[i].y, u[i - 1].y)));
+                    double z = Add(u[i - 1].z, (M)(object)((d_seq - d_comp) * Sub(u[i].z, u[i - 1].z)));
+                    new_points.Add(new Unistroke<M>.Point3<M>((M)(object)x, (M)(object)y, (M)(object)z));
+                    uni.trace.Insert(i + 1, new Unistroke<M>.Point3<M>((M)(object)x, (M)(object)y, (M)(object)z));
+                    d_comp = 0;
+                }
+                else 
+                {
+                    d_comp += d_curr;
+                } 
+            }
+        }
+        
+        //Find the set centroid
+        private void centroid(ref T uni, out Unistroke<M>.Point3<M> centroid) {
+            centroid = new Unistroke<M>.Point3<M>();
+            for (int i = 0; i < uni.trace.Count; i++){
+                centroid.x = (M) (object) Add(uni[i].x, centroid.x);
+                centroid.y = (M) (object) Add(uni[i].y, centroid.y);
+                centroid.z = (M) (object) Add(uni[i].z, centroid.z);
+            }
+
+            centroid.x = (M)(object)((double)(object)centroid.x / uni.trace.Count);
+            centroid.y = (M)(object)((double)(object)centroid.y / uni.trace.Count);
+            centroid.x = (M)(object)((double)(object)centroid.x / uni.trace.Count);
         }
        
-        public int classify(T obj, out int result){
+        //Rotate each point to zero in respect to indicative angle between first point and the set centroid
+        public void rotate_to_zero(ref T uni, out List<Unistroke<M>.Point3<M>> new_points)
+        {
+            new_points = new List<Unistroke<M>.Point3<M>>();
+            Unistroke<M>.Point3<M> center;
+            Unistroke<M> u = uni;
+            centroid(ref uni,out center);
+            double angle = Math.Acos((double) (object) (center * uni[0]) / Mul(center.size(), uni[0].size()));
+            Unistroke<M>.Point3<M> axis = (center ^ uni[0]) / (center ^ uni[0]).size();
+            //Here, the 3d rotation matrix is constructed
+            Unistroke<M>.Matrix3<M> rotation_matrix = new Unistroke<M>.Matrix3<M>();
+            for (int i = 0; i < uni.trace.Count; i++) {
+                new_points[i] = (rotation_matrix * (uni[i] - center)) + center;
+            }
+        }
+
+        //Classify feature vector according to training structures
+        public int classify(T obj, out int result)
+        {
             result = 0;
             return 0;
         }
-
+        
+        //Train classifier by given training
         public int train(L obj){
-            
             return 0;
         }
     }
--- FeatureExtraction/Unistroke.cs
+++ FeatureExtraction/Unistroke.cs
@@ -8,40 +8,89 @@ namespace SignalProcessing
 {
     public class Unistroke<T>
     {
+
         public class Point3<T> : IComparable{
 
-           private static readonly Func<T, T, T> Sub;
+           public static readonly Func<T, T, T> Sub, Add, Mul, Div;
 
-           public T x;
-           public T y;
-           public T z;
+           public T x, y, z;
 
-           Point3(T x, T y, T z) {
+           public Point3() { }
+
+           public Point3(T x, T y, T z) {
                this.x = x;
                this.y = y;
                this.z = z;
-
            }
 
-           static Point3() {
+            
+            static Point3() {
                var firstOperand = Expression.Parameter(typeof(T), "a");
                var secondOperand = Expression.Parameter(typeof(T), "b");
                var body = Expression.Subtract(firstOperand, secondOperand);
+               var body_mul = Expression.Multiply(firstOperand, secondOperand);
+               var body_add = Expression.Add(firstOperand, secondOperand);
+               var body_div = Expression.Divide(firstOperand, secondOperand);
                Sub = Expression.Lambda<Func<T, T, T>>
                      (body, firstOperand, secondOperand).Compile();
+               Mul = Expression.Lambda<Func<T, T, T>>
+                     (body_mul, firstOperand, secondOperand).Compile();
+               Add = Expression.Lambda<Func<T, T, T>>
+                     (body_add, firstOperand, secondOperand).Compile();
+               Div = Expression.Lambda<Func<T, T, T>>
+                     (body_div, firstOperand, secondOperand).Compile();
+           }
+
+           public T size() {
+               return Add(Add(Mul(this.x, this.x), Mul(this.y, this.y)), Mul(this.z, this.z)); 
+           }
+
+           public static T operator *(Point3<T> a, Point3<T> b) {
+               return Add((Add(Mul(a.x, b.x), Mul(a.y, b.y))), Mul(a.z, b.z));
+           }
+
+           public static Point3<T> operator /(Point3<T> a, T b) {
+               return new Point3<T>(Div(a.x, b), Div(a.y, b), Div(a.z, b));
+           }
+
+           public static Point3<T> operator ^(Point3<T> a, Point3<T> b) {
+               return new Point3<T>(Sub(Mul(a.y, b.z), Mul(b.y, a.z)), Sub((T) (object) 0,Sub(Mul(a.x, b.z), Mul(b.x, a.z))), Sub(Mul(a.x, b.y), Mul(b.x, a.y)));
            }
 
            public static Point3<T> operator -(Point3<T> a, Point3<T> b) {
                return new Point3<T>(Sub(a.x, b.x), Sub(a.y, b.y), Sub(a.z, b.z));
            }
+
+           public static Point3<T> operator +(Point3<T> a, Point3<T> b) {
+               return new Point3<T>(Add(a.x, b.x), Add(a.y, b.y), Add(a.z, b.z));
+           }
+
+            public static Point3<T> operator *(Matrix3<T> m, Point3<T> p) {
+                return new Point3<T>(m.vectors[0] * p, m.vectors[1] * p, m.vectors[2] * p);
+            }
             
-            //Compares two points distances from standard 
+            //Compares two points distances off the standard basis 
             public int CompareTo(object p){
                 return 0;
             }
         }
 
+        public class Matrix3<T> {
+            public Point3<T>[] vectors;
+
+            public Matrix3(){
+                vectors = new Point3<T>[3];
+            }
+
+
+        }
+
         public List<Point3<T>> trace;
+
+
+        public Unistroke(Unistroke<T> u){  
+            this.trace = u.trace;
+        }
 
         public Point3<T> this[int i] { 
             get {
